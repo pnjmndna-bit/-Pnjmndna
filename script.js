63f6616170494099c50fdd1e00c9cc455d6a76e6
pnjmndna-bit <pnjmndna@gmail.com>
2026-05-11 16:23:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ document.addEventListener('gesturestart', function(e){
 document.addEventListener('touchmove', function(e){
     e.preventDefault();
 }, { passive:false });
+
+const menuBox = document.querySelectorAll(".menu-box");
+
+menuBox.forEach((box)=>{
+
+    box.addEventListener("click", ()=>{
+
+        // efek ditekan
+        box.style.transform = "scale(0.97)";
+
+        // tampil loading
+        document.getElementById("loadingBox")
+        .style.display = "flex";
+
+        // pindah halaman
+        setTimeout(()=>{
+
+            window.location.href = "pin.html";
+
+        },2000);
+
+    });
+
+});
